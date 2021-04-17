73a924d5e8cccaaa870db645a73ad64ae2e23049
ganpati-bappa <82669393+ganpati-bappa@users.noreply.github.com>
2021-04-17 19:34:48 +0100

    Add files via upload

---
+++ src/components/BloodComponent.js
@@ -0,0 +1,30 @@
+import React, { useState }  from 'react'
+
+//SCSS
+import './../SCSS/BloodComponent.scss'
+
+export const BloodComponent = ({type}) => {
+
+    const [click , setClick] = useState(true)
+
+    const handleClick = () => {
+        setClick(!click)
+    }
+
+    return (
+        <>
+        {click ? (  
+            <div className = "BloodComponent-wrapper" onClick = {handleClick}>
+                {type}
+            </div>
+        ):
+        (
+            <div className = "BloodComponent-clicked " onClick = {handleClick}>
+                {type}
+            </div>)}
+
+        </>
+    )
+}
+
+export default BloodComponent
