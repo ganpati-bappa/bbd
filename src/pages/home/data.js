8007feb200229d456886e67dc4a4b9e651a9f5d6
ganpati-bappa <82669393+ganpati-bappa@users.noreply.github.com>
2021-04-17 16:26:31 +0100

    Update data.js

--- src/pages/home/data.js
+++ src/pages/home/data.js
@@ -1,22 +1,22 @@
 export const homeObjOne = {
-    lightbg: false,
-    lightText: true,
-    lightTextDesc: true,
+    lightbg: true,
+    lightText: false,
+    lightTextDesc: false,
     topline: 'The First Ever',
     headline: 'Blood Bank Database',
-    description: 'Get quick access to all components of blood from your nearest blood blank and save lives of your loved ones.',
+    description: 'Stop Worrying about the inavaliability of Blood, Register and get quick access to all components of blood from your nearest blood blank and save lives of your loved ones. ',
     buttonLabel: 'Register',
     imgStart: '',
     img: "0",
     alt: 'Kyu nhi load ho rhi',
-    link: './sign-up'
+    link: './AuthPage.js'
 };
 
 export const homeObjTwo = {
-    lightbg: true,
-    lightText: false,
-    lightTextDesc: false,
-    topline: 'Quick Access',
+    lightbg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topline: 'Quick Search',
     headline: 'Search for Blood',
     description: 'Search for the requirred components of blood in your nearest Blood Banks in just one click.',
     buttonLabel: 'Search',
@@ -24,4 +24,32 @@ export const homeObjTwo = {
     img: "1",
     alt: 'Kyu nhi load ho rhi',
     link: './Search'
+};
+
+export const homeObjThree = {
+    lightbg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topline: 'Request Blood Camp',
+    headline: 'Organise Blood Camp',
+    description: 'Request for Blood Camp Organisation, gather together and donate blood in order to build a better, disease free India',
+    buttonLabel: 'Register',
+    imgStart: '',
+    img: "2",
+    alt: 'Kyu nhi load ho rhi',
+    link: './OrganiseCampPage.js'
+};
+
+export const homeObjFour = {
+    lightbg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topline: 'Be a Hero',
+    headline: 'Emergency Donate',
+    description: 'Donate blood in Emergency and be a hero for somebody.Every Blood Donor is a hero. Give Blood and save lives',
+    buttonLabel: 'Emergency Donate',
+    imgStart: '',
+    img: "3",
+    alt: 'Kyu nhi load ho rhi',
+    link: './OrganiseCampPage.js'
 };
